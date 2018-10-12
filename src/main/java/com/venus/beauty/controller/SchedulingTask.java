1e746da1a7c3f9f973c760bd23f8015cf2de60d4
xiaoxin <Xiaoxin_llleee@163.com>
2018-10-12 16:31:04 +0800

    4

--- src/main/java/com/venus/beauty/controller/SchedulingTask.java
+++ src/main/java/com/venus/beauty/controller/SchedulingTask.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author lx
@@ -20,9 +21,10 @@ public class SchedulingTask {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 10000)
     public void reportCurrentTime() {
-        jdbcTemplate.update("insert into SCHEDULED value(?)",new Object[]{dateFormat.format(new Date())});
+        jdbcTemplate.update("insert into SCHEDULED value(?,?,?,?)",new Object[]{dateFormat.format(new Date()),
+        "定时任务的demo",UUID.randomUUID().toString(),"task_system"});
         System.out.println("现在时间：" + dateFormat.format(new Date()));
     }
 }
